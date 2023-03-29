86fceaf5cac69fbefc80e84821ed7bdae562dff9
Sakib-Atreus <sakibatreus@gmail.com>
2023-03-30 03:57:18 +0600

    reactjs core-concepts-2 added

--- Milestone 8 - Simple React/Module 47 - React Core Concepts, JSX, props, state/reactjs-core-concepts-2/src/App.js
+++ Milestone 8 - Simple React/Module 47 - React Core Concepts, JSX, props, state/reactjs-core-concepts-2/src/App.js
@@ -1,25 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // const products = [
+  //   {name: "Laptop", price: "130000"},
+  //   {name: "Mobile", price: "30000"},
+  //   {name: "Tablet", price: "50000"},
+  //   {name: "Watch", price: "2000"}
+  // ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {
+        products.map(product => <Product name={product.name} price={product.price}></Product>)
+      }
+
+      <Product name="Laptop" price="700000"></Product>
+      <Product name="Mobile" price="10000"></Product>
+      <Product name="Air-Buds" price="7000"></Product>
+      <Product name="iPhone" price="90000"></Product> */}
+
+      <Counter></Counter>
     </div>
   );
+}
+
+function Counter(){
+  const [count, setCount] = useState(7);
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+  // const increaseCount = () =>{
+  // const newCount = count + 1;
+  // setCount(newCount);
+  // }
+
+  return(
+    <div>
+      <h1>Count : {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
+function Product(props) {
+  return(
+    <div className="product">
+        <h2>Name : {props.name}</h2>
+        <h3>Price : {props.price}</h3>
+    </div>
+  )
 }
 
 export default App;
