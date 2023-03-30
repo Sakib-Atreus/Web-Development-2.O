a33fefb49e0656df9b1966eebb6abe1a8b651b63
Sakib-Atreus <sakibatreus@gmail.com>
2023-03-30 22:20:54 +0600

    React Rest Countries Updated

--- Milestone 8 - Simple React/Module 48 - Simple React Rest Countries/react-rest-countries/src/components/Countries/Countries.js
+++ Milestone 8 - Simple React/Module 48 - Simple React Rest Countries/react-rest-countries/src/components/Countries/Countries.js
@@ -11,7 +11,7 @@ const Countries = () => {
     }, [])
     return (
         <div>
-            <h1>Hello from Countries : {countries.length}</h1>
+            <h1>Hello From Rest Countries : {countries.length}</h1>
             {/* {
                 countries.map(country => <Country 
                     name={country.name.common} 
