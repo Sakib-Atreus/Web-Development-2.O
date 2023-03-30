dafd58049ae4055364dd64fc35b751af700195f9
Sakib-Atreus <sakibatreus@gmail.com>
2023-03-30 06:31:42 +0600

    reactjs core concepts updated

--- Milestone 8 - Simple React/Module 47 - React Core Concepts, JSX, props, state/reactjs-core-concepts-2/src/App.js
+++ Milestone 8 - Simple React/Module 47 - React Core Concepts, JSX, props, state/reactjs-core-concepts-2/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   // const products = [
@@ -20,9 +20,38 @@ function App() {
       <Product name="Air-Buds" price="7000"></Product>
       <Product name="iPhone" price="90000"></Product> */}
 
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+
+      <ExternalUser></ExternalUser>
     </div>
   );
+}
+
+function ExternalUser() {
+  const [users, setUsers] = useState([]);
+  useEffect( () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+  return(
+    <div>
+      <h2>External Users</h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props) {
+  return(
+    <div style={{backgroundColor: "gray", border: "3px solid red", margin: "10px", borderRadius: "20px"}}>
+      <h2>Name : {props.name}</h2>
+      <p>Email : {props.email}</p>
+    </div>
+  )
 }
 
 function Counter(){
