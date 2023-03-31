5774479dbfcb44f5121a44e8c0d858440a2a96f4
Sakib-Atreus <sakibatreus@gmail.com>
2023-03-31 22:15:49 +0600

    react shopping cart added for calculation

--- Milestone 8 - Simple React/Module 51 - Cart Calculation, Save and Retrieve Cart/ema-john-simple-cart-calculation/src/components/Product/Product.jsx
+++ Milestone 8 - Simple React/Module 51 - Cart Calculation, Save and Retrieve Cart/ema-john-simple-cart-calculation/src/components/Product/Product.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css';
 
 const Product = (props) => {
-    const {img, name, seller, quantity, price, ratings} = props.product;
+
+    const { img, name, seller, ratings, price } = props.product;
     const handleAddToCart = props.handleAddToCart;
+
+
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -13,10 +16,10 @@ const Product = (props) => {
                 <h6 className='product-name'>{name}</h6>
                 <p>Price: ${price}</p>
                 <p>Manufacturer: {seller}</p>
-                <p>Rating: {ratings} stars</p>
+                <p>Rating: {ratings} Stars</p>
             </div>
             <button onClick={() => handleAddToCart(props.product)} className='btn-cart'>
-                Add To Cart
+                Add to Cart
                 <FontAwesomeIcon icon={faShoppingCart} />
                 </button>
         </div>
