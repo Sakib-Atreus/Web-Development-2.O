8e359af14c93f91d6bca2c13b75c5bb1c43f05c5
Sakib-Atreus <sakibatreus@gmail.com>
2023-04-24 11:25:49 +0600

    Update firebase

---
+++ Milestone 10 - React Authentication/Module 58 - Simple React Firebase Authentication/simple-firebase/src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmFwDd3vl3DelMGrTWhIbr_r5NAvdKa9Y",
+  authDomain: "simple-firebase-72faf.firebaseapp.com",
+  projectId: "simple-firebase-72faf",
+  storageBucket: "simple-firebase-72faf.appspot.com",
+  messagingSenderId: "902131631530",
+  appId: "1:902131631530:web:bbaa4f0a27dcd98b8897d3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
