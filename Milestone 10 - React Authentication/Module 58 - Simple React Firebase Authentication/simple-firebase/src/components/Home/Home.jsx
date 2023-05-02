ee8c1b4fdf7ca1f9e91d1034888917df2f06a8f9
Sakib-Atreus <sakibatreus@gmail.com>
2023-05-02 10:33:23 +0600

    signInWithPopUp

--- Milestone 10 - React Authentication/Module 58 - Simple React Firebase Authentication/simple-firebase/src/components/Home/Home.jsx
+++ Milestone 10 - React Authentication/Module 58 - Simple React Firebase Authentication/simple-firebase/src/components/Home/Home.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom';
 const Home = () => {
     return (
         <div>
+            <h1>Welcome To HomePage</h1>
+            <Link to="/">Home </Link>
             <Link to="/login">Login</Link>
         </div>
     );
