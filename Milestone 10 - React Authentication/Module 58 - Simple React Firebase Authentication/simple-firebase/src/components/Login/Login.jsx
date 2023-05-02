59f6aac6cc55a83294e53264acc83d565504b7a8
Sakib-Atreus <sakibatreus@gmail.com>
2023-05-02 13:09:24 +0600

    simple firebase done

--- Milestone 10 - React Authentication/Module 58 - Simple React Firebase Authentication/simple-firebase/src/components/Login/Login.jsx
+++ Milestone 10 - React Authentication/Module 58 - Simple React Firebase Authentication/simple-firebase/src/components/Login/Login.jsx
@@ -1,25 +1,67 @@
-import React from 'react';
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import React, { useState } from 'react';
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from '../../firebase/firebase.init';
 
 const Login = () => {
+    const [user, setUser] = useState(null);
+
     const auth = getAuth(app);
-    const provider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider();
 
     const handleGoogleSignIn = () => {
         // console.log('Google mama is coming...');
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, googleProvider)
         .then(result => {
-            const user = result.user;
+            const loggedInUser = result.user;
             console.log(user);
+            setUser(loggedInUser);
         })
         .catch(error => {
             console.log('error', error.message);
         })
     }
+
+    const handleGithubSignIn = () => {
+        signInWithPopup(auth, githubProvider)
+        .then( result => {
+            const loggedUser = result.user;
+            console.log(user);
+            setUser(loggedUser);
+        })
+        .catch(error => {
+            console.log('error', error.message);
+        })
+    }
+    
+    const handleSignOut = () => {
+        signOut(auth)
+            .then( result => {
+                console.log(result);
+                setUser(null);                
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
     return (
         <div>
-            <button onClick={handleGoogleSignIn}>Google Sign IN</button>
+            {
+                user ? 
+                <button onClick={handleSignOut}>Google SignOut</button> :
+                <>
+                    <button onClick={handleGoogleSignIn}>Google SignIn</button> 
+                    <button onClick={handleGithubSignIn}>Github SignIn</button>
+                </>
+            }
+            {user &&
+                <div>
+                    <h3>User : {user.displayName}</h3>
+                    <h3>Email : {user.email}</h3>
+                    <img src={user.photoURL} alt="" />
+                </div>
+            }
         </div>
     );
 };
