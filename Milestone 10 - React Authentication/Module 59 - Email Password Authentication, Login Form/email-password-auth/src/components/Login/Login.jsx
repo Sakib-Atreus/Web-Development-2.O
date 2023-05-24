0ec2f0a0d5d9a2efae0ae41251978f1dd9db10ee
Sakib-Atreus <sakibatreus@gmail.com>
2023-05-24 22:04:10 +0600

    Start again!

--- Milestone 10 - React Authentication/Module 59 - Email Password Authentication, Login Form/email-password-auth/src/components/Login/Login.jsx
+++ Milestone 10 - React Authentication/Module 59 - Email Password Authentication, Login Form/email-password-auth/src/components/Login/Login.jsx
@@ -1,9 +1,56 @@
-import React from 'react';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import app from '../firebase/firebase.config';
+
+const auth = getAuth(app);
 
 const Login = () => {
+        const [error, setError] = useState('');
+        const [success, setSuccess] = useState('');
+
+        const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        signInWithEmailAndPassword(auth, email, password)
+        .then(result => {
+            const loggedUser = result.user;
+            setSuccess('User login successful');
+            setError('');
+        })
+        .catch(error => {
+            setError(error.message);
+        })
+    }
     return (
-        <div>
-            <h2>This is login</h2>
+        <div className='w-25 mx-auto'>
+            
+            <h2>Please login</h2>
+            <Form onSubmit={handleLogin}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" name="email" placeholder="Enter email" required/>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" name="password" placeholder="Password" required/>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Remember me" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+            </Form>
+            <p><small>New to this website? Please <Link to="/register">Register</Link></small></p>
+            <p className='text-danger'>{error}</p>
+            <p className='text-success'>{success}</p>
         </div>
     );
 };
--- Milestone 10 - React Authentication/Module 59 - Email Password Authentication, Login Form/email-password-auth/src/components/Register/Register.jsx
+++ Milestone 10 - React Authentication/Module 59 - Email Password Authentication, Login Form/email-password-auth/src/components/Register/Register.jsx
@@ -1,30 +1,52 @@
 import React, { useState } from 'react';
 import {createUserWithEmailAndPassword, getAuth} from 'firebase/auth'
 import app from '../firebase/firebase.config';
+import { Link } from 'react-router-dom';
 
 const auth = getAuth(app);
 
 const Register = () => {
     const [email, setEmail] = useState('');
+    const [error, setError] = useState('');
+    const [success, setSuccess] = useState('');
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        setSuccess('');
+        setError('');
         const email = event.target.email.value;
         const password = event.target.password.value;
         console.log(email, password)
+        // validate
+        if(!/(?=.*[A-Z])/.test(password)){
+            setError('Please add at least one uppercase');
+            return;
+        }
+        else if(!/(?=.*[0-9]).*[0-9]/.test(password)){
+            setError('Please add at least two numbers');
+            return;
+        }
+        else if(password.length<6){
+            setError('Please add at least 6 characters in your password');
+            return;
+        }
         // create user in firebase
         createUserWithEmailAndPassword(auth, email, password)
         .then(result =>  {
             const loggedUser = result.user;
             console.log(loggedUser);
+            setError('');
+            event.target.reset();
+            setSuccess('User has created successfully');
         })
         .catch(error => {
-            console.log(error)
+            console.log(error.message);
+            setError(error.message);
         })
     }
     const handleEmailChange = (event) => {
         console.log(event.target.value);
-        setEmail(event.target.value);
+        // setEmail(event.target.value);
     }
 
     const handlePasswordBlur = (event) => {
@@ -34,12 +56,16 @@ const Register = () => {
         <div className='w-50 mx-auto'>
             <h2>Register First!!!</h2>
             <form onSubmit={handleSubmit}>
-                <input className='w-50 mb-4 rounded ps-2' onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Enter Your Email'/>
+                <input className='w-50 mb-4 rounded ps-2' onChange={handleEmailChange} type="email" name="email" id="email" placeholder='Enter Your Email' required/>
                 <br />
-                <input className='w-50 mb-4 rounded ps-2' onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Enter Your Password'/>
+                <input className='w-50 mb-4 rounded ps-2' onBlur={handlePasswordBlur} type="password" name="password" id="password" placeholder='Enter Your Password' required/>
                 <br />
                 <input className='btn btn-primary' type="submit" value="Register" />
             </form>
+            <p><small>Already have an account? Please <Link to="/login">Login</Link></small>
+            </p>
+            <p className='text-danger'>{error}</p>
+            <p className='text-success'>{success}</p>
         </div>
     );
 };
